6f72b3d5df50c7cb6f47d1d3ec9d685cd67bbc23
renchooz <rjsharma.rs967@gmail.com>
2025-07-24 14:23:34 +0530

    done

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dbConnection from "./db/db.js";
+import cors from 'cors'
 import UserRoutee from "./Routes/UserRoute.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin:['http://localhost:5173','https://teal-bublanina-36d41f.netlify.app/'],
+    credentials:true
+}))
 dbConnection();
 app.use("/api/user",UserRoutee)
 app.listen(5001, () => {
